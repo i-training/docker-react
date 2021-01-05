28e3aae1ce3eb02995b37dda59f83658dc269dbf
Irshad Ahmad Sheikh <irshad.mike@gmail.com>
2021-01-05 16:31:50 +0530

    aded feature changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world Again..!
+          Hello world Again from feature branch..!
         </p>
         <a
           className="App-link"
